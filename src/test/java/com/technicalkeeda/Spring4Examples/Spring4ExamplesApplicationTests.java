26d468e1aae21c841a3411cd608c8e941ec36c0d
Bertteguen <alberto.h01.ah@gmail.com>
2022-01-31 13:12:38 +0100

    b5r3_DDI

---
+++ src/test/java/com/technicalkeeda/Spring4Examples/Spring4ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicalkeeda.Spring4Examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
